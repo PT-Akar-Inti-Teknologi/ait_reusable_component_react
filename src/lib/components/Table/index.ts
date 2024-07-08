699c912411a198c8357f8684fb3b986d02e01703
muhammad-f-huda-ait <fadkul.huda@akarinti.tech>
2024-07-08 13:28:06 +0700

    build(package_json): update npm build

--- src/lib/components/Table/index.ts
+++ src/lib/components/Table/index.ts
@@ -1,9 +1,15 @@
-export * from './TableBody.page';
-export * from './TableHead.page';
-export * from './TableCell.page';
-export * from './TableRow.page';
+export * from './DraggableTable.page';
+export * from './DraggableTableBody.page';
+export * from './DraggableTableRow.page';
 export * from './Table.page';
+export * from './TableBody.page';
+export * from './TableCell.page';
+export * from './TableHead.page';
+export * from './TableRow.page';
 
+export * from './DraggableTable.types';
+export * from './DraggableTableBody.types';
+export * from './DraggableTableRow.types';
 export * from './Table.types';
 export * from './TableCell.types';
 
