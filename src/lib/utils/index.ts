3a4a020d616875059cfa005ced27147505203f07
muhammad-f-huda-ait <fadkul.huda@akarinti.tech>
2024-09-13 14:15:13 +0700

    feat(canvas_preview_utils): init canvas-preview utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './ait-tailwind-content.utils';
 export * from './array.utils';
+export * from './canvas-preview.utils';
 export * from './file.utils';
 export * from './input.utils';
 export * from './number.utils';
