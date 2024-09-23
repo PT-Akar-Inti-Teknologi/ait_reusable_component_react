162673d285b59f6f97aa64618b066c9dea6517fc
muhammad-f-huda-ait <fadkul.huda@akarinti.tech>
2024-09-23 14:47:57 +0700

    chore(form_data_utils): init form data utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,6 +2,7 @@ export * from './ait-tailwind-content.utils';
 export * from './array.utils';
 export * from './canvas-preview.utils';
 export * from './file.utils';
+export * from './form-data.utils';
 export * from './input.utils';
 export * from './number.utils';
 export * from './object.utils';
